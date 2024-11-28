a2bf0806b51d98337395065f89a628737eff283b
samadkhan1263 <askjr1263@gmail.com>
2024-11-28 14:44:10 +0500

    PF Lab 09

---
+++ task1.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+
+int arr[4][4]={{1,2,3,4},{5,6,7,8},{9,0,1,2},{3,4,5,6}};
+
+for(int i=0;i<4;i++)
+{
+    for(int j=0;j<4;j++)
+  {  cout<<arr[i][j]<<" ";
+
+}
+cout<<endl;
+} 
+return 0;
+}
---
+++ task3.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+int arr[3][4];
+int sum =0;
+for(int i=0;i<3;i++)
+{
+    for(int j=0; j<3;j++)
+    {
+ cout<<"Enter the value"<<"["<<i<<"]"<<"["<<j<<"]";
+ cin>>arr[i][j];
+
+    }
+}
+for (int i=0; i<3;i++)
+{
+  for(int j=0;j<3;j++)
+  {
+   sum+=arr[i][j];
+
+  }
+
+}
+cout<<"Sum is: "<<sum<<endl;
+return 0;
+}
